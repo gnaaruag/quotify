0a6ac93b95a34c6a322af6238376e6e32647a08c
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-04-18 21:56:02 +0530

    static assets

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "../public/quotify.png"
 
+
+
 export default function Home() {
   return (
     <div className='flex flex-col h-[32rem] justify-center items-center mb-0'>
